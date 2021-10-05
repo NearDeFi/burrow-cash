a50ceb8232dcdbaad473db3acb2313ad80c253f8
Esraa Jbara <jbesraa@gmail.com>
2021-10-05 18:22:38 +0300

    add shared folder

---
+++ src/shared/TotalSupply/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material';
+
+const TotalSupply = () => {
+	return (
+		<div style={{ paddingTop: "1em", textAlign: "center", height: "8em" }}>
+			<Button variant="contained" size="large" style={{ height: '6em', width: '14em' }}>
+				{'Total Supply'}
+				<br />{'10,000,000$'}
+			</Button>
+		</div>
+	)
+}
+
+export default TotalSupply;
---
+++ src/shared/index.ts
@@ -0,0 +1,3 @@
+import TotalSupply from "./TotalSupply";
+
+export { TotalSupply };
