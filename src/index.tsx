a0e75afc8a758c02b267775b910ffb21295d050e
Esraa Jbara <jbesraa@gmail.com>
2021-10-07 11:31:49 +0300

    add underline to table row

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { Modal } from './components'
+import './global.css'
 import { initContract } from './utils'
 
 //@ts-ignore
