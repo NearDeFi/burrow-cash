542f216fba61fc4d80fcdbf935b2a9cc38f50811
Esraa Jbara <jbesraa@gmail.com>
2021-10-11 17:00:05 +0300

    style mobile/desktop footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,43 +1,61 @@
+import { Divider } from "@mui/material";
 import Typography from "@mui/material/Typography";
-//@ts-ignore
+import useMobileDetect from "use-mobile-detect-hook";
 import Logo from "../../assets/logo.svg";
-import { Divider, Grid } from "@mui/material"
 
-const SubFooter = () => {
-  //@ts-ignore
+const DesktopFooter = () => {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={6}>
+    <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
+      <div style={{ gridRow: 1, display: "flex", gap: "1em"}}>
         <Typography
           variant="h6"
-          component="div"
           style={{ color: "#00BACF", display: "inline" }}>
-          <Logo/>
+          <Logo />
         </Typography>
-        <Typography style={{ color: "#000741", display: "inline" }}>
+        <Typography variant="h6" style={{ color: "#000741", display: "inline" }}>
           © 2021 All Rights Reserved.
         </Typography>
-      </Grid>
+      </div>
+      <div style={{ gridRow: 1, justifySelf: "end"}}>
+        <Typography variant="h6">
+          <span style={{cursor: "pointer"}}>Terms of Service</span> | <span style={{cursor: "pointer"}}>Privacy Policy</span> 
+        </Typography>
+      </div>
+    </div >
+  );
+};
 
-      <Grid item xs={6} justifyContent={"flex-end"}>
+const MobileFooter = () => {
+  return (
+    <div style={{ display: "grid", gap: "2em" }}>
+      <div style={{ gridRow: 1, display: 'flex', justifyContent: "space-between" }}>
+        <Typography
+          variant="h6"
+          style={{ color: "#00BACF", display: "inline" }}>
+          <Logo />
+        </Typography>
+        <Typography variant="h6" style={{ color: "#000741", display: "inline" }}>
+          © 2021 All Rights Reserved.
+        </Typography>
+      </div>
+
+      <div style={{ gridRow: 2, display: 'flex', justifyContent: "center", gap: "1em" }}>
         <Typography>
           Terms of Service
         </Typography>
-        <Divider orientation={"vertical"} flexItem/>
+        <Divider orientation={"vertical"} flexItem />
         <Typography>
           Privacy Policy
         </Typography>
-      </Grid>
-    </Grid>
+      </div>
+    </div >
   );
 };
 
 const Footer = () => {
-  return (
-    <>
-      <SubFooter/>
-    </>
-  )
+  const detectMobile = useMobileDetect();
+
+  return detectMobile.isMobile() ? <MobileFooter /> : <DesktopFooter />;
 }
 
 export default Footer;
