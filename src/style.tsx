a20d38b4f59d6a7cc906eb95f42ca586eafd6193
Esraa Jbara <jbesraa@gmail.com>
2021-10-12 15:55:21 +0300

    add global style

---
+++ src/style.tsx
@@ -0,0 +1,23 @@
+export const colors = {
+	primary: "#47C880",
+	secondary: "#000741",
+	background: "#F8F9FF",
+	grey: "#D0D0D0",
+	dark: "#000741"
+}
+
+export const typography = {
+}
+
+
+export const Heading6 = ({ cellData = "" }) => {
+	return (
+		<div style={{ color: colors.secondary, fontSize: "12px", fontWeight: 500 }}>{cellData}</div>
+	)
+}
+
+export const Heading4 = ({ cellData = "" }) => {
+	return (
+		<div style={{ color: colors.secondary, fontSize: "14px", fontWeight: 700 }}>{cellData}</div>
+	)
+}
