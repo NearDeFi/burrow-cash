c17345bdf827362ed6966c5333ec78b2bc540ddb
Esraa Jbara <jbesraa@gmail.com>
2021-10-14 13:56:29 +0300

    fix shared

--- src/shared/TotalSupply/index.tsx
+++ src/shared/TotalSupply/index.tsx
@@ -1,23 +1,54 @@
+/* eslint-disable react/prop-types */
+//@ts-ignore
 import { Button } from "@mui/material";
 //@ts-ignore
-import MobileBottomBackground from "../../assets/mobile-bottom.png";
+import useMobileDetect from "use-mobile-detect-hook";
+import DesktopBottomBackground from '../../assets/desktop-bottom-background.jpg';
+import MobileBottomBackground from '../../assets/mobile-bottom.png';
 import { colors } from "../../style";
 
-const TotalSupply = () => {
+
+const DesktopTotalSupplyFooter = ({ height = "10em", displayButton = true }) => {
+	return (
+		<div style={{ display: "grid", justifyContent: "center", height: "24em", backgroundSize: 'cover', backgroundImage: `url(${DesktopBottomBackground})` }}>
+			<div
+				style={{
+					paddingTop: "3em",
+					textAlign: "center",
+					height,
+					width: "250px",
+				}}
+			>
+				<Button
+					variant="contained"
+					size="large"
+					style={{ display: displayButton ? "unset" : "none", height: "100%", width: "100%", backgroundColor: colors.secondary }}
+				>
+					{"Total Supply"}
+					<br />
+					{"10,000,000$"}
+				</Button>
+			</div>
+		</div>
+	);
+};
+
+const MobileTotalSupplyFooter = ({ height = "7em", displayButton = true }) => {
 	return (
 		<div
 			style={{
-				paddingTop: "1em",
+				padding: "2em",
 				textAlign: "center",
-				height: "8em",
-				backgroundSize: "cover",
-				backgroundImage: `url(${MobileBottomBackground})`,
+				display: "grid",
+				justifyContent: "center",
+				height,
+				backgroundSize: 'cover', backgroundImage: `url(${MobileBottomBackground})`
 			}}
 		>
 			<Button
 				variant="contained"
 				size="large"
-				style={{ height: "6em", width: "14em", backgroundColor: colors.secondary }}
+				style={{ display: displayButton ? "unset" : "none", height: "100%", width: "250px", backgroundColor: colors.secondary }}
 			>
 				{"Total Supply"}
 				<br />
@@ -27,4 +58,39 @@ const TotalSupply = () => {
 	);
 };
 
+const TotalSupply = ({ displayButton = true }) => {
+	const detectMobile = useMobileDetect();
+	const isMobile = detectMobile.isMobile();
+
+	if (isMobile) {
+		return <MobileTotalSupplyFooter displayButton={displayButton} />
+	} else {
+		return <DesktopTotalSupplyFooter displayButton={displayButton} />
+	}
+}
 export default TotalSupply;
+
+// const TotalSupply = ({ height = "7em" }) => {
+// 	return (
+// 		<div
+// 			style={{
+// 				paddingTop: "1em",
+// 				textAlign: "center",
+// 				height,
+// 				width: "250px",
+// 				backgroundSize: "cover",
+// 				backgroundImage: `url(${MobileBottomBackground})`,
+// 			}}
+// 		>
+// 			<Button
+// 				variant="contained"
+// 				size="large"
+// 				style={{ height: "100%", width: "100%", backgroundColor: colors.secondary }}
+// 			>
+// 				{"Total Supply"}
+// 				<br />
+// 				{"10,000,000$"}
+// 			</Button>
+// 		</div>
+// 	);
+// };
