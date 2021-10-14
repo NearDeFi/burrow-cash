c75d6939e0526151be6b8bbf066b2ad8006cf1ce
Esraa Jbara <jbesraa@gmail.com>
2021-10-14 14:59:59 +0300

    redirect home to /supply

--- src/router.tsx
+++ src/router.tsx
@@ -1,3 +1,4 @@
+import { Redirect } from 'react-router';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Borrow, Portfolio, Supply } from "./screens";
 
@@ -9,9 +10,7 @@ const AppRouter = () => {
 					<Route
 						exact
 						path="/"
-						component={() => {
-							return <div> Home </div>;
-						}}
+						render={() => <Redirect to="/supply" />}
 					/>
 					<Route path="/supply" component={Supply} />
 					<Route path="/borrow" component={Borrow} />
