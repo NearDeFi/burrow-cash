5c7525e464eed6a80fad358b4002b32ed6a1f92c
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2021-10-20 08:32:41 +0200

    update interface

--- src/config.ts
+++ src/config.ts
@@ -3,23 +3,36 @@ import { ConnectConfig } from "near-api-js";
 export const LOGIC_CONTRACT_NAME = process.env.CONTRACT_NAME || "dev-1634686594172-33981818199919";
 
 export enum ViewMethodsLogic {
-	"get_account",
-	"get_accounts_paged",
-	"get_asset",
-	"get_assets",
-	"get_assets_paged",
-	"get_config",
+	// accounts
+	get_account,
+	get_accounts_paged,
+	// assets
+	get_asset,
+	get_assets,
+	get_assets_paged,
+	// config
+	get_config,
+	// farms
+	get_asset_farm,
+	get_asset_farms,
+	get_asset_farms_paged,
 }
 
 // Change methods can modify the state. But you don't receive the returned value when called.
 export enum ChangeMethodsLogic {
-	"new",
-	"execute",
-	"update_config",
-	"add_asset",
-	"update_asset",
-	"ft_on_transfer",
-	"oracle_on_call",
+	// init
+	new,
+	execute,
+	// config
+	update_config,
+	// assets
+	add_asset,
+	update_asset,
+	ft_on_transfer,
+	oracle_on_call,
+	// farms
+	account_farm_claim_all,
+	add_asset_farm_reward,
 }
 
 export enum ViewMethodsOracle {}
