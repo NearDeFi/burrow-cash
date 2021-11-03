9b3461927a376b4fe496ad504ac467d1132bc22a
ciocan <ciocan@gmail.com>
2021-11-03 14:06:53 +0000

    add log out menu fixes #92

--- src/components/Header/WalletButton.tsx
+++ src/components/Header/WalletButton.tsx
@@ -1,5 +1,7 @@
 import Button from "@mui/material/Button";
-import { useContext } from "react";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useContext, useState } from "react";
 
 import { colors } from "../../style";
 import { IBurrow } from "../../interfaces/burrow";
@@ -8,24 +10,43 @@ import { login, logout } from "../../utils";
 
 const WalletButton = () => {
 	const { walletConnection, account } = useContext<IBurrow>(Burrow);
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
 
-	const onWalletButtonClick = () => {
-		if (walletConnection.isSignedIn()) {
-			logout(walletConnection);
-		} else {
+	const onWalletButtonClick = (event) => {
+		if (!walletConnection.isSignedIn()) {
 			login(walletConnection);
+		} else {
+			setAnchorEl(event.currentTarget);
 		}
 	};
 
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
 	return (
-		<Button
-			size="small"
-			style={{ justifySelf: "end", backgroundColor: colors.primary }}
-			variant="contained"
-			onClick={onWalletButtonClick}
-		>
-			{walletConnection.isSignedIn() ? account.accountId : "Connect Wallet"}
-		</Button>
+		<div>
+			<Button
+				size="small"
+				style={{ justifySelf: "end", backgroundColor: colors.primary }}
+				variant="contained"
+				onClick={onWalletButtonClick}
+			>
+				{walletConnection.isSignedIn() ? account.accountId : "Connect Wallet"}
+			</Button>
+			<Menu
+				id="profile-menu"
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+				MenuListProps={{
+					"aria-labelledby": "logout-button",
+				}}
+			>
+				<MenuItem onClick={() => logout(walletConnection)}>Log Out</MenuItem>
+			</Menu>
+		</div>
 	);
 };
 
