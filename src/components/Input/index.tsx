a77604ba9f747cb728d31ed23c1f09cb4031aff9
ciocan <ciocan@gmail.com>
2021-11-05 15:13:20 +0000

    implement max button / fixes #47

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,20 +1,27 @@
 import IconButton from "@mui/material/IconButton";
 import InputAdornment from "@mui/material/InputAdornment";
 import OutlinedInput from "@mui/material/OutlinedInput";
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 
 import MaxIcon from "./max.svg";
 
 interface inputFieldProps {
 	type?: string;
 	value?: any;
-	onChange?: (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => void;
+	max: number;
+	onChange?: (value: number) => void;
 }
 
 /** initial version, should be updated as needed */
 const InputField = (props: inputFieldProps) => {
-	const { value, type, onChange, ...rest } = props;
+	const { value, type, onChange, max, ...rest } = props;
 	const [controlledValue, setControlledValue] = useState<any>(value);
+
+	const handleMax = () => {
+		setControlledValue(max);
+		if (onChange) onChange(max);
+	};
+
 	return (
 		<OutlinedInput
 			id="input-box"
@@ -25,12 +32,12 @@ const InputField = (props: inputFieldProps) => {
 			inputProps={{ min: 0 }}
 			onChange={(e) => {
 				setControlledValue(e.target.value);
-				if (onChange) onChange(e);
+				if (onChange) onChange(Number(e.target.value));
 			}}
 			{...rest}
 			endAdornment={
 				<InputAdornment position="end">
-					<IconButton aria-label="max value" onClick={() => null}>
+					<IconButton aria-label="max value" onClick={handleMax}>
 						<MaxIcon />
 					</IconButton>
 				</InputAdornment>
