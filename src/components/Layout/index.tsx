166397f5a04641ef05ee74bf0312581cd8fcb5bd
chez <chez>
2021-11-06 19:45:53 +0200

    FIX: Background SVG Image Scaling Issue

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,23 @@
 import { Box } from "@mui/material";
-
+import * as React from "react";
+// @ts-ignore
+import useMobileDetect from "use-mobile-detect-hook";
 import Footer from "../Footer";
 import Header from "../Header";
-import Background from "./bg.svg";
+import BackgroundDesktop from "./bg-desktop.svg";
+import BackgroundMobile from "./bg-mobile.svg";
 
 const Layout = ({ children }) => {
+	const detectMobile = useMobileDetect();
+	const Background = detectMobile.isMobile() ? (
+		<BackgroundMobile width="100%" />
+	) : (
+		<BackgroundDesktop width="100%" />
+	);
 	return (
 		<Box sx={{ position: "relative" }}>
-			<Box sx={{ position: "fixed", left: 0, right: 0, top: 0, bottom: 0, zIndex: 0 }}>
-				<Background width="100%" />
+			<Box sx={{ position: "absolute", left: 0, right: 0, top: "-20px", bottom: 0, zIndex: 0 }}>
+				{Background}
 			</Box>
 			<Box
 				sx={{
