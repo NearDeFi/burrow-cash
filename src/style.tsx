b74ce95c8adc8a936c85ff1194322213ed1901d2
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2021-11-16 09:04:30 +0100

    remove dead code

--- src/style.tsx
+++ src/style.tsx
@@ -5,17 +5,3 @@ export const colors = {
   grey: "#D0D0D0",
   dark: "#000741",
 };
-
-export const typography = {};
-
-export const Heading6 = ({ cellData = "" }) => {
-  return (
-    <div style={{ color: colors.secondary, fontSize: "12px", fontWeight: 500 }}>{cellData}</div>
-  );
-};
-
-export const Heading4 = ({ cellData = "" }) => {
-  return (
-    <div style={{ color: colors.secondary, fontSize: "14px", fontWeight: 700 }}>{cellData}</div>
-  );
-};
