726d61534cc246829199a6263393128571bdf53d
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2021-11-16 15:51:27 +0100

    make withdraw able to withdraw all the funds fixed token decimals when withdrawing USDT some other changes

--- src/screens/Portfolio/index.tsx
+++ src/screens/Portfolio/index.tsx
@@ -10,7 +10,7 @@ import Table from "../../components/Table";
 import { suppliedColumns, borrowColumns } from "./tabledata";
 
 const Portfolio = () => {
-  const { assets, metadata, portfolio } = useContext(ContractContext);
+  const { assets, metadata, portfolio, balances } = useContext(ContractContext);
   const theme = useTheme();
 
   const totalSupplied = portfolio?.supplied
@@ -35,7 +35,7 @@ const Portfolio = () => {
     .filter(
       (asset) =>
         !!portfolio?.supplied.find((s) => s.token_id === asset.token_id) ||
-        !!portfolio?.collateral.find((s) => s.token_id === asset.token_id),
+        !!portfolio?.collateral.find((c) => c.token_id === asset.token_id),
     )
     .map((asset) => ({
       ...asset,
@@ -49,6 +49,7 @@ const Portfolio = () => {
   const borrowRows = portfolio?.borrowed.map((borrowed) => ({
     ...assets.find((m) => m.token_id === borrowed.token_id),
     ...metadata.find((a) => a.token_id === borrowed.token_id),
+    wallet: balances.find((b) => b.token_id === borrowed.token_id),
     ...borrowed,
   }));
 
@@ -64,7 +65,7 @@ const Portfolio = () => {
         <span style={{ color: theme.palette.primary.main }}>Supplied</span> Assets
       </Typography>
 
-      {portfolio?.supplied.length ? (
+      {portfolio?.supplied.length || portfolio?.collateral.length ? (
         <Table rows={suppliedRows} columns={suppliedColumns} />
       ) : (
         <div style={{ textAlign: "center" }}>No supplied assets yet</div>
