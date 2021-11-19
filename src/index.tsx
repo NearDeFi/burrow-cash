75d08fcca76bfb344a3368d5d576174b87ece6f7
ciocan <ciocan@gmail.com>
2021-11-19 11:55:00 +0000

    add coming soon message

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "@mui/material/styles";
+import { Box } from "@mui/material";
+// import { ThemeProvider } from "@mui/material/styles";
 
-import App from "./App";
-import { Modal } from "./components";
-import { ContractContextProvider } from "./context/contracts";
-import "./global.css";
-import { initContract } from "./utils";
+// import App from "./App";
+// import { Modal } from "./components";
+// import { ContractContextProvider } from "./context/contracts";
+// import { initContract } from "./utils";
+// import theme from "./theme";
 import { IBurrow } from "./interfaces/burrow";
-import theme from "./theme";
+import "./global.css";
 
 export const Burrow = React.createContext<IBurrow>({} as IBurrow);
 
+const ComingSoon = () => (
+  <Box display="flex" alignItems="center" height="100vh" justifyContent="center">
+    <Box>coming soon...</Box>
+  </Box>
+);
+
+ReactDOM.render(<ComingSoon />, document.querySelector("#root"));
+
 // @ts-ignore
-window.nearInitPromise = initContract()
-  .then((initResults) => {
-    ReactDOM.render(
-      <Burrow.Provider value={initResults}>
-        <ThemeProvider theme={theme}>
-          <Modal>
-            <ContractContextProvider>
-              <App />
-            </ContractContextProvider>
-          </Modal>
-        </ThemeProvider>
-      </Burrow.Provider>,
-      document.querySelector("#root"),
-    );
-  })
-  .catch(console.error);
+// window.nearInitPromise = initContract()
+//   .then((initResults) => {
+//     ReactDOM.render(
+//       <Burrow.Provider value={initResults}>
+//         <ThemeProvider theme={theme}>
+//           <Modal>
+//             <ContractContextProvider>
+//               <App />
+//             </ContractContextProvider>
+//           </Modal>
+//         </ThemeProvider>
+//       </Burrow.Provider>,
+//       document.querySelector("#root"),
+//     );
+//   })
+//   .catch(console.error);
