07e852a2cdd8dc7effa7b2c85bddb07152011d21
ciocan <ciocan@gmail.com>
2021-11-19 14:33:50 +0000

    upgrade react router to v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import "regenerator-runtime/runtime";
-import { Redirect } from "react-router";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navigate } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Borrow, Portfolio, Supply, Terms, Privacy } from "./screens";
 import { Layout } from "./components";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Layout>
-          <Route exact path="/" render={() => <Redirect to="/supply" />} />
-          <Route path="/supply" component={Supply} />
-          <Route path="/borrow" component={Borrow} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/terms" component={Terms} />
-          <Route path="/privacy" component={Privacy} />
-        </Layout>
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/supply" />} />
+          <Route path="/supply" element={<Supply />} />
+          <Route path="/borrow" element={<Borrow />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy" element={<Privacy />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 };
 
