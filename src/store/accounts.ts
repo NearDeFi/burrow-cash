adb103742354508d746e9868b6626c9ad6d35e42
Radu Ciocan <ciocan@gmail.com>
2021-12-10 10:11:24 +0000

    remove unused

--- src/store/accounts.ts
+++ src/store/accounts.ts
@@ -1,14 +1,5 @@
-import {
-  IAccount,
-  IAccountDetailed,
-  IBalance,
-  ViewMethodsLogic,
-  IMetadata,
-  IAssetDetailed,
-} from "../interfaces";
+import { IAccount, IAccountDetailed, ViewMethodsLogic } from "../interfaces";
 import { getBurrow } from "../utils";
-import { shrinkToken } from "./helper";
-import { getBalance } from "./tokens";
 
 export const getAccounts = async (): Promise<IAccount[]> => {
   const { view, logicContract } = await getBurrow();
@@ -45,49 +36,6 @@ export const getAccountsDetailed = async (): Promise<IAccountDetailed[]> => {
   ).filter((account): account is IAccountDetailed => !!account);
 
   return result;
-};
-
-export const getPortfolio = async (
-  metadata: IMetadata[],
-  assets: IAssetDetailed[],
-): Promise<IAccountDetailed | undefined> => {
-  const { account } = await getBurrow();
-
-  const accountDetailed: IAccountDetailed | null = await getAccountDetailed(account.accountId!);
-
-  if (accountDetailed) {
-    const acc = JSON.parse(JSON.stringify(accountDetailed));
-
-    for (const asset of [...acc.supplied, ...acc.borrowed, ...acc.collateral]) {
-      const { decimals } = metadata.find((m) => m.token_id === asset.token_id)!;
-      const { config } = assets.find((a) => a.token_id === asset.token_id)!;
-      const d = decimals + config.extra_decimals;
-      // asset.shares = shrinkToken(asset.shares, d);
-      asset.balance = shrinkToken(asset.balance, d);
-    }
-
-    return acc;
-  }
-  return undefined;
-};
-
-export const getBalances = async (token_ids: string[]): Promise<IBalance[]> => {
-  const { account, walletConnection } = await getBurrow();
-
-  const balances: IBalance[] = await Promise.all(
-    token_ids.map(
-      async (token_id) =>
-        ({
-          token_id,
-          account_id: account.accountId,
-          balance:
-            (walletConnection?.isSignedIn() && (await getBalance(token_id, account.accountId))) ||
-            0,
-        } as IBalance),
-    ),
-  );
-
-  return balances;
 };
 
 export const getAccount = async () => {
