37cb78720fd49108398a107a4c90e0fb85cebfce
Radu Ciocan <ciocan@gmail.com>
2022-02-16 07:49:54 -0700

    add spinners

--- src/components/TotalBRRR/index.tsx
+++ src/components/TotalBRRR/index.tsx
@@ -1,19 +1,33 @@
-import { Box, Alert } from "@mui/material";
+import { Box, Alert, Skeleton } from "@mui/material";
 
 import { getTotalBRRR } from "../../redux/accountSelectors";
 import { useAppSelector } from "../../redux/hooks";
 import { TOKEN_FORMAT } from "../../store";
+import { useLoading } from "../../hooks";
 
 export default function TotalBRRR() {
   const [total, unclaimed] = useAppSelector(getTotalBRRR);
+  const isLoading = useLoading();
 
   return (
     <Box width={["100%", "520px"]} mx="auto" mb="2rem">
       <Alert severity="info">
-        You&apos;ve earned: &nbsp;
-        <b>{total.toLocaleString(undefined, TOKEN_FORMAT)} BRRR</b>&nbsp;
-        <span>(unclaimed: {unclaimed.toLocaleString(undefined, TOKEN_FORMAT)})</span>
-        <span>&nbsp;&nbsp; 🎉 💪 🌒</span>
+        <Box display="flex" justifyItems="center">
+          <span>You&apos;ve earned: &nbsp;</span>
+          {isLoading ? (
+            <Skeleton sx={{ bgcolor: "gray", justifyItems: "center" }} width={80} />
+          ) : (
+            <b>{total.toLocaleString(undefined, TOKEN_FORMAT)} BRRR</b>
+          )}
+          &nbsp;
+          <span>(unclaimed: &nbsp;</span>
+          {isLoading ? (
+            <Skeleton sx={{ bgcolor: "gray", justifyItems: "center" }} width={80} />
+          ) : (
+            <span>{unclaimed.toLocaleString(undefined, TOKEN_FORMAT)}</span>
+          )}
+          <span>)&nbsp;&nbsp; 🎉 💪 🌒</span>
+        </Box>
       </Alert>
     </Box>
   );
