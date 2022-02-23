c16ee679fb77e5c923da914bd13eef75a0283e67
Radu Ciocan <ciocan@gmail.com>
2022-02-22 19:34:24 -0700

    fix banner

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,25 +1,30 @@
-import { Box, Link, Alert } from "@mui/material";
+import { Box, Alert, Link } from "@mui/material";
+
 import Footer from "../Footer";
 import Header from "../Header";
+
+const Banner = () => (
+  <Alert severity="success" sx={{ pl: ["20px", "28px"] }}>
+    <Link
+      href="https://burrowcash.medium.com/printer-goes-betabrrr-b7faa2ae9bd3"
+      title="Printer goes betaBRRR"
+      target="_blank"
+    >
+      We’re announcing betaBRRR, a very, very worthless test token for the public beta release!
+    </Link>
+  </Alert>
+);
 
 const Layout = ({ children }) => (
   <Box
     sx={{
       display: "grid",
-      gridTemplateRows: "auto 1fr auto",
+      gridTemplateRows: "auto auto 1fr auto",
       gridTemplateColumns: "100%",
       minHeight: "100%",
     }}
   >
-    <Alert severity="success" sx={{ pl: ["20px", "28px"] }}>
-      <Link
-        href="https://burrowcash.medium.com/printer-goes-betabrrr-b7faa2ae9bd3"
-        title="Printer goes betaBRRR"
-        target="_blank"
-      >
-        We’re announcing betaBRRR, a very, very worthless test token for the public beta release!
-      </Link>
-    </Alert>
+    <Banner />
     <Header />
     <main>{children}</main>
     <Footer />
