b1308d16cbbd8c06db14116f05188934c996cedb
Radu Ciocan <ciocan@gmail.com>
2022-04-16 10:59:27 +0200

    add token info

--- src/components/Ticker/index.tsx
+++ src/components/Ticker/index.tsx
@@ -2,32 +2,45 @@ import { Box, Stack, Link } from "@mui/material";
 import TimeAgo from "timeago-react";
 
 import { getFeedEvents } from "../../redux/feedSelectors";
+import { shrinkToken } from "../../store/helper";
+import { TOKEN_FORMAT } from "../../store/constants";
 import { getAssets } from "../../redux/assetsSelectors";
 import { useAppSelector } from "../../redux/hooks";
+import TokenIcon from "../TokenIcon";
 import FeedData from "./feed";
 import "./styles.css";
 
 const Ticker = () => {
   const events = useAppSelector(getFeedEvents);
   const assets = useAppSelector(getAssets);
-  console.log(assets);
 
   return (
     <Box className="ticker">
       <Stack className="ticker-list" spacing={2} direction="row">
-        {events.map((e) => (
-          <Stack key={e.receiptId} spacing={2} py="0.5rem" direction="row">
-            <Link
-              href={`https://app.burrow.cash?viewAs=${e.data.accountId}`}
-              target="blank"
-              fontWeight="bold"
-            >
-              {e.data.accountId}
-            </Link>
-            <TimeAgo className="time-ago" datetime={new Date(e.timestamp / 1e6)} />
-            <Box>{e.event}</Box>
-          </Stack>
-        ))}
+        {events.map((e) => {
+          const asset = assets[e.data.tokenId];
+          const decimals = asset.metadata.decimals + asset.config.extra_decimals;
+          const amount = Number(shrinkToken(e.data.amount, decimals)).toLocaleString(
+            undefined,
+            TOKEN_FORMAT,
+          );
+
+          return (
+            <Stack key={e.receiptId} spacing={2} py="0.5rem" direction="row">
+              <Link
+                href={`https://app.burrow.cash?viewAs=${e.data.accountId}`}
+                target="blank"
+                fontWeight="bold"
+              >
+                {e.data.accountId}
+              </Link>
+              <TimeAgo className="time-ago" datetime={new Date(e.timestamp / 1e6)} />
+              <Box>{e.event}</Box>
+              <TokenIcon width={18} height={18} icon={asset.metadata.icon} />
+              <Box>{amount}</Box>
+            </Stack>
+          );
+        })}
       </Stack>
       <FeedData />
     </Box>
