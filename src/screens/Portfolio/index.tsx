9cc72d730a542531f10b1dfc279d9b1e8e0dc570
Radu Ciocan <ciocan@gmail.com>
2022-04-25 16:40:13 +0100

    persist table sorting

--- src/screens/Portfolio/index.tsx
+++ src/screens/Portfolio/index.tsx
@@ -5,10 +5,12 @@ import Table from "../../components/Table";
 import { suppliedColumns, borrowedColumns } from "./tabledata";
 import { useAppSelector } from "../../redux/hooks";
 import { getPortfolioAssets, getAccountId } from "../../redux/accountSelectors";
+import { useTableSorting } from "../../hooks";
 
 const Portfolio = () => {
   const [suppliedRows, borrowedRows] = useAppSelector(getPortfolioAssets);
   const accountId = useAppSelector(getAccountId);
+  const { sorting, setSorting } = useTableSorting();
 
   return (
     <Box pb="2.5rem" display="grid" justifyContent="center">
@@ -20,8 +22,8 @@ const Portfolio = () => {
         <Table
           rows={suppliedRows}
           columns={suppliedColumns}
-          sortColumn="supplied"
           sx={{ maxWidth: "800px", width: "none" }}
+          sorting={{ name: "portfolioDeposited", ...sorting.portfolioDeposited, setSorting }}
         />
       ) : (
         <Box textAlign="center">No deposited assets yet</Box>
@@ -31,8 +33,8 @@ const Portfolio = () => {
         <Table
           rows={borrowedRows}
           columns={borrowedColumns}
-          sortColumn="borrowed"
           sx={{ maxWidth: "800px", width: "none" }}
+          sorting={{ name: "portfolioBorrowed", ...sorting.portfolioBorrowed, setSorting }}
         />
       ) : (
         <Box textAlign="center">No borrowed assets yet</Box>
