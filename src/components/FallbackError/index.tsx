409546d1f194a8082c9870000e8c92cc39131110
Radu Ciocan <ciocan@gmail.com>
2022-05-04 22:38:21 +0100

    fallback error with reload session

--- src/components/FallbackError/index.tsx
+++ src/components/FallbackError/index.tsx
@@ -1,13 +1,21 @@
-import { Alert, Box, Stack, Typography } from "@mui/material";
+import { Alert, Box, Stack, Typography, Button } from "@mui/material";
 
 import SadHog from "./sad-hog.svg";
 
 const FallbackError = ({ error }) => {
+  const resetSession = () => {
+    localStorage.removeItem("persist:root");
+    window.location.reload();
+  };
+
   return (
-    <Stack direction="column" alignItems="center" justifyContent="center" height="100vh">
+    <Stack gap={0.5} direction="column" alignItems="center" justifyContent="center" height="100vh">
       <SadHog />
       <Typography fontSize="1rem">Ooops! Something went wrong.</Typography>
-      <Box maxWidth="400px" mt={2}>
+      <Button size="small" variant="contained" onClick={resetSession}>
+        Click here to reload your session
+      </Button>
+      <Box maxWidth="400px" mt="1rem">
         <Alert severity="error">
           <Typography color="red" fontSize="0.7rem">
             {error.message}
