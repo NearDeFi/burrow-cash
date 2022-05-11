d13a5e61412fcdce4cd6d6ed7eb33f436138ecef
Radu Ciocan <ciocan@gmail.com>
2022-05-11 22:03:11 +0100

    add borrows for new daily amount

--- src/redux/selectors/getAccountRewards.ts
+++ src/redux/selectors/getAccountRewards.ts
@@ -35,75 +35,82 @@ export const getAccountRewards = createSelector(
     const brrrTokenId = app.config.booster_token_id;
     const { xBRRR, extraXBRRRAmount } = staking;
 
-    const computeRewards = ([tokenId, farm]: [string, Farm]) => {
-      return Object.entries(farm).map(([rewardTokenId, farmData]) => {
-        const asset = assets.data[tokenId];
-        const assetDecimals = asset.metadata.decimals + asset.config.extra_decimals;
-        const rewardAsset = assets.data[rewardTokenId];
-        const rewardAssetDecimals =
-          rewardAsset.metadata.decimals + rewardAsset.config.extra_decimals;
+    const computeRewards =
+      (type) =>
+      ([tokenId, farm]: [string, Farm]) => {
+        return Object.entries(farm).map(([rewardTokenId, farmData]) => {
+          const asset = assets.data[tokenId];
+          const assetDecimals = asset.metadata.decimals + asset.config.extra_decimals;
+          const rewardAsset = assets.data[rewardTokenId];
+          const rewardAssetDecimals =
+            rewardAsset.metadata.decimals + rewardAsset.config.extra_decimals;
 
-        const totalRewardsPerDay = Number(
-          shrinkToken(farmData.asset_farm_reward.reward_per_day, rewardAssetDecimals),
-        );
+          const totalRewardsPerDay = Number(
+            shrinkToken(farmData.asset_farm_reward.reward_per_day, rewardAssetDecimals),
+          );
 
-        const totalBoostedShares = Number(
-          shrinkToken(farmData.asset_farm_reward.boosted_shares, assetDecimals),
-        );
+          const totalBoostedShares = Number(
+            shrinkToken(farmData.asset_farm_reward.boosted_shares, assetDecimals),
+          );
 
-        const boostedShares = Number(shrinkToken(farmData.boosted_shares, assetDecimals));
-        const { icon, symbol, name } = rewardAsset.metadata;
+          const boostedShares = Number(shrinkToken(farmData.boosted_shares, assetDecimals));
+          const { icon, symbol, name } = rewardAsset.metadata;
 
-        const dailyAmount = (boostedShares / totalBoostedShares) * totalRewardsPerDay;
-        const unclaimedAmount = Number(shrinkToken(farmData.unclaimed_amount, rewardAssetDecimals));
+          const dailyAmount = (boostedShares / totalBoostedShares) * totalRewardsPerDay;
+          const unclaimedAmount = Number(
+            shrinkToken(farmData.unclaimed_amount, rewardAssetDecimals),
+          );
 
-        // new daily amount
-        const boosterLogBase = Number(
-          shrinkToken(farmData.asset_farm_reward.booster_log_base, app.config.booster_decimals),
-        );
+          // new daily amount
+          const boosterLogBase = Number(
+            shrinkToken(farmData.asset_farm_reward.booster_log_base, app.config.booster_decimals),
+          );
 
-        const xBRRRAmount = xBRRR + extraXBRRRAmount;
-        const log = Math.log(xBRRRAmount) / Math.log(boosterLogBase);
-        const multiplier = log >= 0 ? 1 + log : 1;
+          const xBRRRAmount = xBRRR + extraXBRRRAmount;
+          const log = Math.log(xBRRRAmount) / Math.log(boosterLogBase);
+          const multiplier = log >= 0 ? 1 + log : 1;
 
-        const suppliedShares = Number(
-          shrinkToken(account.portfolio.supplied[asset.token_id]?.shares || 0, assetDecimals),
-        );
-        const collateralShares = Number(
-          shrinkToken(account.portfolio.collateral[asset.token_id]?.shares || 0, assetDecimals),
-        );
-        const shares = suppliedShares + collateralShares;
-        const newBoostedShares = shares * multiplier;
-        const newTotalBoostedShares = totalBoostedShares + newBoostedShares - boostedShares;
-        const newDailyAmount = (newBoostedShares / newTotalBoostedShares) * totalRewardsPerDay;
+          const suppliedShares = Number(
+            shrinkToken(account.portfolio.supplied[asset.token_id]?.shares || 0, assetDecimals),
+          );
+          const collateralShares = Number(
+            shrinkToken(account.portfolio.collateral[asset.token_id]?.shares || 0, assetDecimals),
+          );
+          const borrowedShares = Number(
+            shrinkToken(account.portfolio.borrowed[asset.token_id]?.shares || 0, assetDecimals),
+          );
+          const shares = type === "supplied" ? suppliedShares + collateralShares : borrowedShares;
+          const newBoostedShares = shares * multiplier;
+          const newTotalBoostedShares = totalBoostedShares + newBoostedShares - boostedShares;
+          const newDailyAmount = (newBoostedShares / newTotalBoostedShares) * totalRewardsPerDay;
 
-        console.info(
-          asset.token_id,
-          assetDecimals,
-          multiplier,
-          rewardAsset.token_id,
-          dailyAmount,
-          newDailyAmount,
-        );
+          console.info(
+            asset.token_id,
+            assetDecimals,
+            multiplier,
+            rewardAsset.token_id,
+            dailyAmount,
+            newDailyAmount,
+          );
 
-        return {
-          icon,
-          name,
-          symbol,
-          tokenId: rewardTokenId,
-          unclaimedAmount,
-          dailyAmount,
-          boosterLogBase,
-          newDailyAmount,
-          multiplier,
-        };
-      });
-    };
+          return {
+            icon,
+            name,
+            symbol,
+            tokenId: rewardTokenId,
+            unclaimedAmount,
+            dailyAmount,
+            boosterLogBase,
+            newDailyAmount,
+            multiplier,
+          };
+        });
+      };
 
     const { supplied, borrowed } = account.portfolio.farms;
 
-    const suppliedRewards = Object.entries(supplied).map(computeRewards).flat();
-    const borrowedRewards = Object.entries(borrowed).map(computeRewards).flat();
+    const suppliedRewards = Object.entries(supplied).map(computeRewards("supplied")).flat();
+    const borrowedRewards = Object.entries(borrowed).map(computeRewards("borrowed")).flat();
 
     const sumRewards = [...suppliedRewards, ...borrowedRewards].reduce((rewards, asset) => {
       if (!rewards[asset.tokenId]) return { ...rewards, [asset.tokenId]: asset };
