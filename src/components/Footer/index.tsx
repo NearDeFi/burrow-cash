df777ec366cb3aa025b4dbb62daa4ed6695278f6
Radu Ciocan <ciocan@gmail.com>
2022-05-17 08:57:53 +0100

    fix discord link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,27 +5,12 @@ import { FaTwitter } from "@react-icons/all-files/fa/FaTwitter";
 import { FaMedium } from "@react-icons/all-files/fa/FaMedium";
 import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
 
-// import BackgroundFooter from "./bg-footer.svg";
 import Logo from "../../assets/logo.svg";
 import Gitbook from "../../assets/GitBook.svg";
 import { Wrapper, CopyWrapper, LinksWrapper, LogoWrapper, Copyright } from "./style";
 
 const Footer = () => (
   <Wrapper>
-    {/*
-    <Box
-      sx={{
-        position: "absolute",
-        left: 0,
-        right: 0,
-        bottom: 0,
-        zIndex: "-1",
-        pointerEvents: "none",
-      }}
-    >
-      <BackgroundFooter />
-    </Box>
-    */}
     <CopyWrapper>
       <LogoWrapper>
         <Logo />
@@ -43,18 +28,13 @@ const Links = () => (
     display="grid"
     gridTemplateColumns="repeat(5, 1fr)"
     alignItems="center"
-    gap={["0.5rem", "1rem"]}
     lineHeight="0"
+    sx={{ gap: ["0.5rem", "1rem"] }}
   >
     <Link href="https://github.com/NearDeFi/" title="GitHub" target="_blank" color="#000">
       <FaGithub />
     </Link>
-    <Link
-      href="https://discord.com/channels/912857183455154196/912857184247889992"
-      title="Discord"
-      target="_blank"
-      color="#000"
-    >
+    <Link href="https://discord.gg/8esAssuE" title="Discord" target="_blank" color="#000">
       <FaDiscord />
     </Link>
     <Link href="https://twitter.com/burrowcash" title="Twitter" target="_blank" color="#000">
@@ -63,7 +43,7 @@ const Links = () => (
     <Link href="https://burrowcash.medium.com/" title="Medium" target="_blank" color="#000">
       <FaMedium />
     </Link>
-    <Link width="16px" href="https://docs.burrow.cash/" title="Docs" target="_blank" color="#000">
+    <Link href="https://docs.burrow.cash/" title="Docs" target="_blank" color="#000" width="16px">
       <Gitbook />
     </Link>
   </Box>
