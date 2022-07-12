ac4b25909c09232a42ee9fe7639d52f92c326e39
Radu Ciocan <ciocan@gmail.com>
2022-07-12 11:28:08 +0100

    add label for net rewards

--- src/components/Header/stats/rewards.tsx
+++ src/components/Header/stats/rewards.tsx
@@ -2,29 +2,35 @@ import { useRewards } from "../../../hooks/useRewards";
 import { TOKEN_FORMAT, USD_FORMAT } from "../../../store";
 import { Stat } from "./components";
 
+const transformReward = (r) => ({
+  value: r.dailyAmount.toLocaleString(undefined, TOKEN_FORMAT),
+  unclaimed: `${r.unclaimedAmount.toLocaleString(undefined, TOKEN_FORMAT)} unclaimed`,
+  text: r.symbol,
+  icon: r.icon,
+});
+
+const sumRewards = (acc, r) => acc + r.dailyAmount * r.price;
+
 export const DailyRewards = () => {
   const { brrr, extra, net } = useRewards();
 
-  const rewards = [
+  const assetRewards = [
     ...(Object.entries(brrr).length > 0 ? [brrr] : []),
     ...extra.flatMap((f) => f[1]),
-    ...net.flatMap((f) => f[1]),
   ];
 
-  const labels = rewards.map((r) => ({
-    value: r.dailyAmount.toLocaleString(undefined, TOKEN_FORMAT),
-    unclaimed: `${r.unclaimedAmount.toLocaleString(undefined, TOKEN_FORMAT)} unclaimed`,
-    text: r.symbol,
-    icon: r.icon,
-  }));
+  const netRewards = net.flatMap((f) => f[1]);
 
-  const amount = rewards.reduce((acc, r) => acc + r.dailyAmount * r.price, 0);
+  const labels = assetRewards.map(transformReward);
+  const netLabels = netRewards.map(transformReward);
+
+  const amount = assetRewards.reduce(sumRewards, 0) + netRewards.reduce(sumRewards, 0);
 
   return (
     <Stat
       title="Daily Rewards"
       amount={amount.toLocaleString(undefined, USD_FORMAT)}
-      labels={labels}
+      labels={[...labels, { text: "Net Liquidity:" }, ...netLabels]}
     />
   );
 };
