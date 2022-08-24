d09565f081a8162a4501b167f71326a894f684c2
Radu Ciocan <ciocan@gmail.com>
2022-08-24 23:14:49 +0100

    fix prices

--- src/store/assets.ts
+++ src/store/assets.ts
@@ -3,7 +3,6 @@ import Decimal from "decimal.js";
 import { IAssetEntry, IAssetDetailed, AssetEntry, ViewMethodsLogic } from "../interfaces";
 import { getBurrow } from "../utils";
 import { DEFAULT_PRECISION } from "./constants";
-import { getPrices } from "./helper";
 
 Decimal.set({ precision: DEFAULT_PRECISION });
 
@@ -34,15 +33,6 @@ export const getAssetDetailed = async (token_id: string): Promise<IAssetDetailed
 
 export const getAssetsDetailed = async (): Promise<IAssetDetailed[]> => {
   const assets: IAssetEntry[] = await getAssets();
-
-  const priceResponse = await getPrices();
-  let detailedAssets = await Promise.all(assets.map((asset) => getAssetDetailed(asset.token_id)));
-
-  detailedAssets = detailedAssets?.map((detailedAsset) => ({
-    ...detailedAsset,
-    price:
-      priceResponse?.prices.find((p) => p.asset_id === detailedAsset.token_id)?.price || undefined,
-  }));
-
+  const detailedAssets = await Promise.all(assets.map((asset) => getAssetDetailed(asset.token_id)));
   return detailedAssets;
 };
