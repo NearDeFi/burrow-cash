58b4e5b2f715355449b026a119ba07b9bcc5b972
Radu Ciocan <ciocan@gmail.com>
2022-09-23 15:22:03 +0100

    add test func isReactNative for walletconnect2

--- src/utils.ts
+++ src/utils.ts
@@ -204,3 +204,7 @@ export function decimalMin(a: string | number | Decimal, b: string | number | De
   b = new Decimal(b);
   return a.lt(b) ? a : b;
 }
+
+export function isReactNative(): boolean {
+  return false;
+}
