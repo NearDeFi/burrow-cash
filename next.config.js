e4ff001ae9112e9e7e76e3a7634b0b3339d47830
Radu Ciocan <ciocan@gmail.com>
2022-10-20 20:45:11 +0100

    add exportTrailingSlash to next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  exportTrailingSlash: true,
   webpack(config, { isServer, webpack, buildId }) {
     config.plugins.push(
       new webpack.DefinePlugin({
