d04bc905271ccd8f6756d55c20bf2f5bfe69fb45
Radu Ciocan <ciocan@gmail.com>
2022-10-20 21:02:14 +0100

    fix menu highlight

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  exportTrailingSlash: true,
+  trailingSlash: true,
   webpack(config, { isServer, webpack, buildId }) {
     config.plugins.push(
       new webpack.DefinePlugin({
